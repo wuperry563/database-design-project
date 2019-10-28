141a8820dbc6d4fae324d61729f3e5541c1122b1
perry wu <wuperry563@gmail.com>
2019-10-27 19:38:21 -0500

    feat(sql): query search

---
+++ express-postgres/queries.js
@@ -0,0 +1,45 @@
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'postgres',
+  password: 'pg',
+  port: 54320,
+})
+
+const getUsers = (request, response) => {
+    pool.query('SELECT * FROM name', (error, results) => {
+      if (error) {
+        response.status(400).json(error)
+      }
+      else{
+        response.status(200).json(results.rows)
+      }
+    })
+  }
+
+const searchQuery = (request, response) => {
+    const queryString = `SELECT book.isbn, title, author.name,\
+    not exists(select * from book_loan where book_loan.isbn=book.isbn and date_in < now())
+    as can_checkout\
+     FROM book\
+    join book_author on book.isbn=book_author.isbn\
+    join author on book_author.author_id = author.author_id\
+    WHERE \
+    to_tsvector(title) ||\
+    to_tsvector(book.isbn) ||\
+    to_tsvector(author.name) @@\
+    to_tsquery(\'${request.query.searchString}\');`
+    pool.query(queryString, (error, results)=>{
+        if (error) {
+            response.status(400).json(error)
+          }
+          else{
+            response.status(200).json(results.rows)
+          }
+    })
+}
+
+module.exports={
+    getUsers, searchQuery
+}
--- express-postgres/routes/index.js
+++ express-postgres/routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+const db = require('../queries')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.json({ info: 'Node.js, Express, and Postgres API' })
 });
+
+router.get('/users', db.getUsers)
+
+router.get('/search', db.searchQuery)
 
 module.exports = router;
